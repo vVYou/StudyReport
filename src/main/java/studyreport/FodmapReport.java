34380562c2b63c46a3772f143df1f657e8df6d52
vVYou <vincent.vanouytsel@gmail.com>
2020-11-17 21:30:47 +0100

    FodmapReport implementation

--- src/main/java/studyreport/FodmapReport.java
+++ src/main/java/studyreport/FodmapReport.java
@@ -1,17 +1,19 @@
 package studyreport;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.ArrayUtils;
 
 import studyreport.fodmap.AnswerType;
 import studyreport.fodmap.FodmapAnswer;
@@ -22,13 +24,18 @@ public class FodmapReport {
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		String fodmapDataInputFile = "..\\data\\testnov\\FODMAP.csv";
+		List<FodmapAnswer> answers = getFodmapAnswers(fodmapDataInputFile);
 
+		for (FodmapGroup fodmapGroup : FodmapGroup.values()) {
+			writeGroupReport(fodmapGroup, answers);
+		}
+	}
+
+	private static List<FodmapAnswer> getFodmapAnswers(String fodmapDataInputFile) throws URISyntaxException, IOException {
 		List<FodmapAnswer> answers = new ArrayList<>();
-
-		FileReader fileReader = ReportUtils.getFileReader(fodmapDataInputFile);
-		try (CSVParser csvParser = new CSVParser(fileReader, ReportUtils.getFormat(FodmapCSVDescription.class))) {
+		try (CSVParser csvParser = new CSVParser(ReportUtils.getFileReader(fodmapDataInputFile), ReportUtils.getFormat(FodmapCSVDescription.class))) {
 			for (CSVRecord record : csvParser.getRecords()) {
-				FodmapAnswer fodmapAnswer = new FodmapAnswer(record.get(FodmapCSVDescription.numero_d_identification));
+				FodmapAnswer fodmapAnswer = new FodmapAnswer(record.get(FodmapCSVDescription.numero_d_identification.getColumnId()));
 				for (FodmapCSVDescription description : FodmapCSVDescription.values()) {
 					if (description.getType() == AnswerType.FOOD) {
 						fodmapAnswer.addAnswer(description, ReportUtils.toInt(record.get(description)));
@@ -37,10 +44,7 @@ public class FodmapReport {
 				answers.add(fodmapAnswer);
 			}
 		}
-
-		for (FodmapGroup fodmapGroup : FodmapGroup.values()) {
-			writeGroupReport(fodmapGroup, answers);
-		}
+		return answers;
 	}
 
 	private static void writeGroupReport(FodmapGroup fodmapGroup, List<FodmapAnswer> answers) throws IOException {
@@ -48,15 +52,38 @@ public class FodmapReport {
 		FileWriter fileWriter = new FileWriter(new File(outputReportFile), false);
 		CSVPrinter reportPrinter = new CSVPrinter(fileWriter, CSVFormat.EXCEL);
 
-		List<Object> headers = new ArrayList<>();
-		headers.addAll();
-		reportPrinter.printRecord(headers.toArray());
+		List<FodmapCSVDescription> fodmapGroupDescriptions = getFodmapGroupDescriptions(fodmapGroup);
+
+		reportPrinter.printRecord(getHeaders(fodmapGroupDescriptions).toArray());
 
 		for (FodmapAnswer answer : answers) {
-			reportPrinter.printRecord(values);
+			reportPrinter.printRecord(getRecord(fodmapGroupDescriptions, answer));
 		}
 
 		reportPrinter.close();
+	}
+
+	private static List<Object> getRecord(List<FodmapCSVDescription> fodmapGroupDescriptions, FodmapAnswer answer) {
+		List<Object> record = new ArrayList<>();
+		record.add(answer.getStudyId());
+		for (FodmapCSVDescription fodmapGroupDescription : fodmapGroupDescriptions) {
+			record.add(answer.getAnswers().get(fodmapGroupDescription));
+		}
+		return record;
+	}
+
+	private static List<Object> getHeaders(List<FodmapCSVDescription> fodmapGroupDescriptions) {
+		List<Object> headers = new ArrayList<>();
+		headers.add(FodmapCSVDescription.numero_d_identification);
+		headers.addAll(fodmapGroupDescriptions);
+		return headers;
+	}
+
+	private static List<FodmapCSVDescription> getFodmapGroupDescriptions(FodmapGroup fodmapGroup) {
+		return Arrays.stream(FodmapCSVDescription.values())
+				.filter(description -> ArrayUtils.contains(description.getGroups(), fodmapGroup))
+				.sorted()
+				.collect(Collectors.toList());
 	}
 
 }
